baa2ab09ceae2a852094b26de24d497f29a59f67
KTKin <www.happyktk@gmail.com>
2025-01-13 20:09:02 +0800

    record ROI coordination with .txt

--- ClickEvent.py
+++ ClickEvent.py
@@ -1,11 +1,29 @@
 import cv2
 
-
 def mouse(event, x, y, flags, param): # click event to print coordination
+    # left button: record coordinates
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(int(x*(img_ow/img_rw)), int(y*(img_oh/img_rh)))
+        temp_x=int(x*(img_ow/img_rw))
+        temp_y=int(y*(img_oh/img_rh))
+        print(temp_x,temp_y)
+        f=open("ROI_coord.txt","a")
+        string=str(temp_x)+" "+str(temp_y)+" "
+        f.write(string)
+        f.close()
+    # right button: clear
+    if event == cv2.EVENT_RBUTTONDOWN:
+        print("cleared")
+        f=open("ROI_coord.txt","w")
+        f.write("")
+        f.close()
+    # middle button: display current coordinates
+    if event == cv2.EVENT_MBUTTONDOWN:
+        f=open("ROI_coord.txt","r")
+        coord = f.read().split()
+        print(coord)
 
-img = cv2.imread("pic2.jpg")
+cap = cv2.VideoCapture("video.mp4")
+ret, img = cap.read()
 img_oh = img.shape[0] # original height
 img_ow = img.shape[1] # original width
 img_rh = 720 # resize height
@@ -14,7 +32,8 @@ img_rw = 1200 # resize width
 frame = cv2.resize(img, (img_rw,img_rh))
 cv2.imshow("frame", frame)
 
-
 cv2.setMouseCallback('frame', mouse)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+# press q to quit
+if cv2.waitKey(0) & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
